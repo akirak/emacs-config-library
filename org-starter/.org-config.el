28b1823b21091ee101da1452d57857df0f4b9840
Akira Komamura <akira.komamura@gmail.com>
2019-10-29 10:40:21 +0900

    Typo

--- org-starter/.org-config.el
+++ org-starter/.org-config.el
@@ -43,7 +43,7 @@ Skips capture tasks, projects, and subprojects."
 
 (setq-default org-clock-in-switch-to-state #'akirak/clock-in-to-next)
 
-;;;; Org files in this repositories
+;;;; Org files in this repository
 ;;;;; yankpad.org
 (org-starter-define-file (expand-file-name "yankpad/yankpad.org" no-littering-etc-directory)
   :key "y"
